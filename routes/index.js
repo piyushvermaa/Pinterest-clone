30a1e01c691dd666b2d78a269c6c4d16c5362169
piyushvermaa <piyush.verma_cs22@gla.ac.in>
2024-03-17 00:25:20 +0530

    image upload feature added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const userModel= require('./users');
 const postModel= require('./posts');
 const passport = require('passport');
+const upload = require('./multer');
+
 const localStrategy= require('passport-local');
 passport.use(new localStrategy(userModel.authenticate()));
 
@@ -18,10 +20,29 @@ router.get('/login', function(req, res, next) {
 router.get('/feed', isLoggedIn,function(req, res, next) {
   res.render('feed');
 });
+
+router.post('/upload', isLoggedIn,upload.single("file"), async function(req, res, next){
+  if(!req.file){
+    return res.status(404).send("No files were given.");
+  }
+  const user=await userModel.findOne({username: req.session.passport.user});
+  const post=await postModel.create({
+    image: req.file.filename,
+    postText: req.body.filecaption,
+    user: user._id
+  });
+
+   user.posts.push(post._id);
+   await user.save();
+   res.send('done.')
+});
+
+
 router.get('/profile', isLoggedIn, async function(req, res, next) {
   const user= await userModel.findOne({
     username: req.session.passport.user,
   })
+  .populate("posts");
   res.render('profile', {user: user});
 });
 
